30119e74f24f8c56e8d9061f2ffe1b832fe57dbc
Jackbusters <77648717+JackHTerrell@users.noreply.github.com>
2025-04-25 03:57:19 -0500

    1.16.5 Full

---
+++ src/main/java/com/jackbusters/turtlesdropscute/TurtlesDropScute.java
@@ -0,0 +1,19 @@
+package com.jackbusters.turtlesdropscute;
+
+import com.jackbusters.turtlesdropscute.glm.GeneralSerializer;
+import net.minecraftforge.common.loot.GlobalLootModifierSerializer;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+@Mod(TurtlesDropScute.MOD_ID)
+public class TurtlesDropScute {
+    public static final String MOD_ID = "turtlesdropscute";
+    public static final DeferredRegister<GlobalLootModifierSerializer<?>> LOOT_MODIFIERS = DeferredRegister.create(ForgeRegistries.LOOT_MODIFIER_SERIALIZERS, MOD_ID);
+    public static final RegistryObject<GlobalLootModifierSerializer<?>> ADD_DROP = LOOT_MODIFIERS.register("add_drop", GeneralSerializer::new);
+    public TurtlesDropScute(){
+        LOOT_MODIFIERS.register(FMLJavaModLoadingContext.get().getModEventBus());
+    }
+}
---
+++ src/main/java/com/jackbusters/turtlesdropscute/glm/EpicLootModifier.java
@@ -0,0 +1,54 @@
+package com.jackbusters.turtlesdropscute.glm;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.loot.LootContext;
+import net.minecraft.loot.conditions.ILootCondition;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.loot.LootModifier;
+
+import javax.annotation.Nonnull;
+import java.util.List;
+
+/**
+ * <h1>Global Loot Modifier (GLM)</h1>
+ * <p>A loot modifier system built to easily modify any loot table on a whim.</p>
+ * <p>Specific loot modifications are data-driven. This is simply the logic behind how the fields in each json are applied.</p>
+ * <p>Different Loot Table modifications are created and can be previewed in data/epicadditions/loot_modifiers.
+ * All must then be registered with Forge in data/forge/loot_modifiers/global_loot_modifers.json.</p>
+ * <p>Loot modifications should <strong>never</strong> be the result of replacing a loot table. Doing so would break other mods' loot.</p>
+ */
+public class EpicLootModifier extends LootModifier {
+
+    public final Item item; // The item to generate
+    public final int amount; // the number of items
+    public final ILootCondition[] conditionsIn;
+    public final String lootTable; // the loot table to generate the loot in (i.e. "minecraft:entities/wither").
+    public final double chance; // The chance of the loot generating 0 being no chance, 1 being guaranteed.
+
+    protected EpicLootModifier(ILootCondition[] conditionsIn, Item item, int amount, String lootTable, double chance) {
+        super(conditionsIn);
+        this.conditionsIn = conditionsIn;
+        this.item = item;
+        this.amount = amount;
+        this.lootTable = lootTable;
+        this.chance = chance;
+    }
+
+    @Nonnull
+    @Override
+    protected List<ItemStack> doApply(List<ItemStack> list, LootContext lootContext) {
+        if (lootContext.getQueriedLootTableId().equals(ResourceLocation.tryParse(lootTable))) {
+            ItemStack itemStack = new ItemStack(item, amount);
+
+            if (chance == 1)
+                list.add(itemStack);
+            else {
+                double random = Math.random();
+                if (random < chance)
+                    list.add(itemStack);
+            }
+        }
+        return list;
+    }
+}
