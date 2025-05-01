b2bc1e7d035c41436ef2ee7ea2f2d4bd07b0a4f6
Jackbusters <77648717+JackHTerrell@users.noreply.github.com>
2025-04-30 20:18:37 -0500

    1.20.6 Full

--- src/main/java/com/jackbusters/turtlesdropscute/TurtlesDropScute.java
+++ src/main/java/com/jackbusters/turtlesdropscute/TurtlesDropScute.java
@@ -5,17 +5,18 @@ import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
 import net.minecraft.entity.EntityType;
 import net.minecraft.item.Items;
 import net.minecraft.loot.LootPool;
+import net.minecraft.loot.LootTable;
 import net.minecraft.loot.entry.ItemEntry;
-import net.minecraft.util.Identifier;
+import net.minecraft.registry.RegistryKey;
 
 public class TurtlesDropScute implements ModInitializer {
-    private static final Identifier TURTLE_LOCATION = EntityType.TURTLE.getLootTableId();
+    private static final RegistryKey<LootTable> TURTLE_LOCATION = EntityType.TURTLE.getLootTableId();
     @Override
     public void onInitialize() {
-        LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
-            if (source.isBuiltin() && TURTLE_LOCATION.equals(id)) {
-                LootPool.Builder poolBuilder = LootPool.builder().with(ItemEntry.builder(Items.SCUTE));
-                tableBuilder.pool(poolBuilder);
+        LootTableEvents.MODIFY.register((key, table, source) -> {
+            if (source.isBuiltin() && TURTLE_LOCATION.equals(key)) {
+                LootPool.Builder poolBuilder = LootPool.builder().with(ItemEntry.builder(Items.TURTLE_SCUTE));
+                table.pool(poolBuilder);
             }
         });
     }
