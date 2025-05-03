3e098678beea6912e39d3ba70865502ef8bb73df
Jackbusters <77648717+JackHTerrell@users.noreply.github.com>
2025-05-02 19:47:03 -0500

    1.21.5 Full

--- src/main/java/com/jackbusters/turtlesdropscute/glm/EpicLootModifier.java
+++ src/main/java/com/jackbusters/turtlesdropscute/glm/EpicLootModifier.java
@@ -9,6 +9,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.storage.loot.LootContext;
+import net.minecraft.world.level.storage.loot.LootTable;
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
 import net.minecraftforge.common.loot.IGlobalLootModifier;
 import net.minecraftforge.common.loot.LootModifier;
@@ -54,7 +55,7 @@ public class EpicLootModifier extends LootModifier {
 
     @Nonnull
     @Override
-    protected ObjectArrayList<ItemStack> doApply(ObjectArrayList<ItemStack> generatedLoot, LootContext context) {
+    protected ObjectArrayList<ItemStack> doApply(LootTable table, ObjectArrayList<ItemStack> generatedLoot, LootContext context) {
         if (context.getQueriedLootTableId().equals(ResourceLocation.tryParse(lootTable))) {
             ItemStack itemStack = new ItemStack(() -> item, amount);
 
